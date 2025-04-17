4e3cfeaf2927a847cae263730a25ef2d2e25e145
Aishchan <72770826+Aishchan@users.noreply.github.com>
2025-04-17 11:58:58 -0600

    Update ACCESSBikeComfort_Backend.py

--- ACCESSBikeComfort_Backend.py
+++ ACCESSBikeComfort_Backend.py
@@ -19,19 +19,6 @@ from shapely.ops import nearest_points
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('ACCESSBikeComfort_Webtool.html')  # Serve frontend
-
-@app.route('/route', methods=['POST'])
-def route():
-    # your routing logic...
-    return jsonify({'result': 'routing result'})
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
 # Set environment variable to restore .shx file
 os.environ['SHAPE_RESTORE_SHX'] = 'YES'
 
